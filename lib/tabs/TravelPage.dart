ec4e35de10e2a058baba5b6cc543b1487e7a7ace
jiangbowen_ <18853716897@163.com>
2020-05-31 14:57:46 +0800

    下拉刷新

--- lib/tabs/TravelPage.dart
+++ lib/tabs/TravelPage.dart
@@ -10,6 +10,7 @@ class TravelPage extends StatefulWidget {
 class _NewState extends State<TravelPage> {
   TuChongRepository tuChongRepository = TuChongRepository();
   bool loadSuccess = false;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -19,26 +20,24 @@ class _NewState extends State<TravelPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-            flex: 1,
-            child: StreamBuilder(
-              builder: (context, snapshot) {
-                List<TuChongItem> list = snapshot.data;
-                return _listViewCustom(list);
-              },
-              stream: tuChongRepository.rebuild,
-              initialData: null,
-            ))
-      ],
-    );
+    return RefreshIndicator(
+        child: StreamBuilder(
+          builder: (context, snapshot) {
+            List<TuChongItem> list = snapshot.data;
+            return _listViewCustom(list);
+          },
+          stream: tuChongRepository.rebuild,
+          initialData: null,
+        ),
+        onRefresh: () => _initData());
   }
 
   Widget _listViewCustom(List<TuChongItem> data) {
     int itemCount = 20;
     data == null ? itemCount : (itemCount = data.length);
     return ListView.builder(
+        physics: AlwaysScrollableScrollPhysics(), //确保视图始终可以滚动
+//        controller: _scrollController(),
         itemCount: itemCount,
         itemBuilder: (context, index) {
           return ListTile(
@@ -48,7 +47,12 @@ class _NewState extends State<TravelPage> {
             leading: data == null
                 ? Image.asset("images/xiecheng_active.png",
                     width: 62, height: 62)
-                : Image.network(data[index].images[0].imageUrl,width: 100,height: 80 ,fit: BoxFit.fill,),
+                : Image.network(
+                    data[index].images[0].imageUrl,
+                    width: 100,
+                    height: 80,
+                    fit: BoxFit.fill,
+                  ),
             title: data == null
                 ? Text(" $loadSuccess flutter hello world!!!!")
                 : Text(data[index].title),
@@ -56,7 +60,16 @@ class _NewState extends State<TravelPage> {
         });
   }
 
-  void _initData() async {
-     loadSuccess = await tuChongRepository.refresh();
+
+  ScrollController _scrollController(){
+    ScrollController scrollController = ScrollController();
+    var offset = scrollController.offset;
+    print("---------offset");
+    return scrollController;
+  }
+
+  Future<void> _initData() async {
+    loadSuccess = await tuChongRepository.refresh();
+    if (loadSuccess) {}
   }
 }
